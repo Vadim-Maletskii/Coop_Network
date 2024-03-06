3b786a90ad54dd0ec4ed066371675342facb2fdd
skorotkikh <s.korotkikh21@gmail.com>
2024-03-06 21:49:13 +0100

    switching lines for node attributes

--- scripts/starting_code.R
+++ scripts/starting_code.R
@@ -8,29 +8,20 @@ prices <- read.csv('data_orig/supermarket_prices.csv', sep = ' ')
 
 purchases <- merge(purchases, prices, by = "product_id", all.x = TRUE)
 
-cusprod <- purchases %>% filter(shop_id == 1) %>%  select(customer_id, product_id)
-cusprod$customer_id <- paste("C", cusprod$customer_id, sep = "")
-cusprod$product_id <- paste("P", cusprod$product_id, sep = "")
-
-distances$customer_id <- paste("C", distances$customer_id, sep = "")
-distances$shop_id <- paste("S", distances$shop_id, sep = "")
-
 purchases$customer_id <- paste("C", purchases$customer_id, sep = "")
 purchases$product_id <- paste("P", purchases$product_id, sep = "")
 purchases$shop_id <- paste("S", purchases$shop_id, sep = "")
 
+distances$customer_id <- paste("C", distances$customer_id, sep = "")
+distances$shop_id <- paste("S", distances$shop_id, sep = "")
+
 purchases <- merge(purchases, distances, by = c("customer_id", "shop_id"), all.x = TRUE)
 
+cusprod <- purchases %>% select(customer_id, product_id)
 cusprod_matrix <- as.matrix(cusprod)
 cusprod_net <- graph_from_edgelist(cusprod_matrix)
 
-# cusprod_net <- graph_from_data_frame(cusprod, vertices = cusprod$customer_id)
-
-edge_density(cusprod_net)
-sort(degree(cusprod_net, mode='in'), decreasing=T)
-degree_distribution(cusprod_net)
-
-# cusprod_net %>% 
+# cusprod_net %>%  
 #   degree(mode = "in") %>% 
 #   hist(,
 #        col = "red",
@@ -39,6 +30,10 @@ degree_distribution(cusprod_net)
 #        main = "Support",
 #        breaks = min(.):max(.)
 #   )
+
+# edge_density(cusprod_net)
+# sort(degree(cusprod_net, mode='in'), decreasing=T)
+# degree_distribution(cusprod_net)
 
 
 #Node attributes ----
