2279ae1ddf0702b431193dbee91cd10042696dff
Vadim Maletskii <wiscain@yandex.ru>
2024-03-07 10:04:39 +0100

    some_code_management

---
+++ scripts/data_preparation.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(igraph)
+library(ggraph)
+
+purchases <- read.csv('data_orig/supermarket_purchases.csv', sep = ' ')
+distances <- read.csv('data_orig/supermarket_distances.csv', sep = ' ')
+prices <- read.csv('data_orig/supermarket_prices.csv', sep = ' ')
+
+purchases <- merge(purchases, prices, by = "product_id", all.x = TRUE)
+
+purchases$customer_id <- paste("C", purchases$customer_id, sep = "")
+purchases$product_id <- paste("P", purchases$product_id, sep = "")
+purchases$shop_id <- paste("S", purchases$shop_id, sep = "")
+
+distances$customer_id <- paste("C", distances$customer_id, sep = "")
+distances$shop_id <- paste("S", distances$shop_id, sep = "")
+
+purchases <- merge(purchases, distances, by = c("customer_id", "shop_id"), all.x = TRUE)
+
+
