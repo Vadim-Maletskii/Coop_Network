1534216ce875126fb6dd3896a20bfb3f966643fc
skorotkikh <s.korotkikh21@gmail.com>
2024-03-14 21:53:04 +0100

    H1 + H2 whole df with charts

--- scripts/h1.R
+++ scripts/h1.R
@@ -41,12 +41,18 @@ pie_chart_percent_in_shops <- ggplot(quantity_percent_in_shops, aes(x = "", y = 
   scale_fill_brewer()
 
 # H2 whole df:
-sampled_df %>% group_by(customer_id, shop_category) %>% summarize(sum_spent = sum(quantity * price)) %>% mutate(percent = sum_spent / sum(sum_spent) * 100)
-sum_spent_grouped <- sampled_df %>%
-  group_by(customer_id, shop_category) %>%
-  summarize(sum_products = sum(quantity)) %>%
-  mutate(percentage = (sum_products / sum(sum_products)) * 100)
-sum_p_grouped %>% group_by(shop_category) %>% summarize(avg_percent = mean(percentage))
+sum_spent_grouped <- whole_df %>% group_by(customer_id, shop_category) %>%
+  summarize(sum_spent = sum(quantity * price)) %>% mutate(percent = sum_spent / sum(sum_spent) * 100)
+sum_percent_in_shops <- sum_spent_grouped %>% group_by(shop_category) %>% summarize(avg_percent = round(mean(percent), 2))
+
+pie_chart_spent_in_shops <- ggplot(sum_percent_in_shops, aes(x = "", y = avg_percent, fill = shop_category)) +
+  geom_bar(stat = "identity", width = 1) +
+  geom_text(aes(label = paste0(avg_percent, "%")), position = position_stack(vjust = 0.5)) +  # Add text labels for avg_percent
+  coord_polar("y", start = 0) +  # Convert bar plot to pie chart
+  labs(title = "Distribution of Money Spent (%) by Customer in Each Shop") +
+  theme_void() +  # Remove unnecessary elements
+  theme(legend.position = "right", plot.title = element_text(hjust = 0.5)) +  # Adjust legend position if needed
+  scale_fill_brewer(palette = 'Greens')
 
 # Sampled df ----
 sampled_df <- sampled_df %>%
