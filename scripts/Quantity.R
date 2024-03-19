b385d6b20ffa485bd29bf7cb2c929fb4ba7895da
Vadim Maletskii <wiscain@yandex.ru>
2024-03-19 23:02:25 +0100

    Graphs

--- scripts/Quantity.R
+++ scripts/Quantity.R
@@ -40,4 +40,211 @@ shop_id_S3 <- distinct (shop_id_S3)
 shop_id_S4 <- distinct (shop_id_S4)
 shop_id_S5 <- distinct (shop_id_S5)
 
+set.seed(793)
+shop_id_S1 <- shop_id_S1[sample(nrow(shop_id_S4), 250), ]
+shop_id_S2 <- shop_id_S2[sample(nrow(shop_id_S4), 250), ]
+shop_id_S3 <- shop_id_S3[sample(nrow(shop_id_S4), 250), ]
+shop_id_S4 <- shop_id_S4[sample(nrow(shop_id_S4), 250), ]
+shop_id_S5 <- shop_id_S5[sample(nrow(shop_id_S4), 250), ]
+
+category_colors = c("red", "blue", "green","orange","purple")
+
+#----
+# Create a list of unique customer IDs and shop categories
+unique_customers <- unique(shop_id_S1 $customer_id)
+unique_categories <- unique(shop_id_S1 $shop_category)
+
+# Create an empty adjacency matrix with dimension names
+adj_matrix <- matrix(0, nrow = length(unique_customers), 
+                     ncol = length(unique_customers),
+                     dimnames = list(unique_customers, unique_customers))
+
+# Loop through the dataframe to update adjacency matrix
+for (i in 1:nrow(shop_id_S1 )) {
+  customer1 <- shop_id_S1 $customer_id[i]
+  category <- shop_id_S1 $shop_category[i]
+  
+  # Find other customers with the same category
+  other_customers <- shop_id_S1 $customer_id[shop_id_S1 $shop_category == category & 
+                                              shop_id_S1 $customer_id != customer1]
+  
+  # Update adjacency matrix for each pair of customers with the same category
+  adj_matrix[customer1, other_customers] <- 1
+}
+
+# Convert adjacency matrix to igraph object
+g <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected", weighted = TRUE)
+
+# Set diversity as node sizes
+V(g)$size <- shop_id_S1 $diversity
+V(g)$colour <- as.factor (shop_id_S1$shop_category)
+
+# Plot with manual colors
+ggraph(g, layout = "fr") +
+  geom_edge_link(color = "white") +
+  geom_node_point(aes(size = size, colour = colour), alpha = 0.5) +
+  scale_size_continuous(range = c(0.5, 3)) + 
+  scale_colour_manual(values = category_colors) +
+  theme_void()
+
+#----
+# Create a list of unique customer IDs and shop categories
+unique_customers <- unique(shop_id_S2 $customer_id)
+unique_categories <- unique(shop_id_S2 $shop_category)
+
+# Create an empty adjacency matrix with dimension names
+adj_matrix <- matrix(0, nrow = length(unique_customers), 
+                     ncol = length(unique_customers),
+                     dimnames = list(unique_customers, unique_customers))
+
+# Loop through the dataframe to update adjacency matrix
+for (i in 1:nrow(shop_id_S2 )) {
+  customer1 <- shop_id_S2 $customer_id[i]
+  category <- shop_id_S2 $shop_category[i]
+  
+  # Find other customers with the same category
+  other_customers <- shop_id_S2 $customer_id[shop_id_S2 $shop_category == category & 
+                                               shop_id_S2 $customer_id != customer1]
+  
+  # Update adjacency matrix for each pair of customers with the same category
+  adj_matrix[customer1, other_customers] <- 1
+}
+
+# Convert adjacency matrix to igraph object
+g <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected", weighted = TRUE)
+
+# Set diversity as node sizes
+V(g)$size <- shop_id_S2 $diversity
+V(g)$colour <- as.factor (shop_id_S2$shop_category)
+
+
+# Plot with manual colors
+ggraph(g, layout = "fr") +
+  geom_edge_link(color = "white") +
+  geom_node_point(aes(size = size, colour = colour), alpha = 0.5) +
+  scale_size_continuous(range = c(0.5, 3)) + 
+  scale_colour_manual(values = category_colors) +
+  theme_void()
+
+
+#----
+
+# Create a list of unique customer IDs and shop categories
+unique_customers <- unique(shop_id_S3 $customer_id)
+unique_categories <- unique(shop_id_S3 $shop_category)
+
+# Create an empty adjacency matrix with dimension names
+adj_matrix <- matrix(0, nrow = length(unique_customers), 
+                     ncol = length(unique_customers),
+                     dimnames = list(unique_customers, unique_customers))
+
+# Loop through the dataframe to update adjacency matrix
+for (i in 1:nrow(shop_id_S3 )) {
+  customer1 <- shop_id_S3 $customer_id[i]
+  category <- shop_id_S3 $shop_category[i]
+  
+  # Find other customers with the same category
+  other_customers <- shop_id_S3 $customer_id[shop_id_S3 $shop_category == category & 
+                                               shop_id_S3 $customer_id != customer1]
+  
+  # Update adjacency matrix for each pair of customers with the same category
+  adj_matrix[customer1, other_customers] <- 1
+}
+
+# Convert adjacency matrix to igraph object
+g <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected", weighted = TRUE)
+
+# Set diversity as node sizes
+V(g)$size <- shop_id_S3 $diversity
+V(g)$colour <- as.factor (shop_id_S3$shop_category)
+
+# Plot with manual colors
+ggraph(g, layout = "fr") +
+  geom_edge_link(color = "white") +
+  geom_node_point(aes(size = size, colour = colour), alpha = 0.5) +
+  scale_size_continuous(range = c(0.5, 3)) + 
+  scale_colour_manual(values = category_colors) +
+  theme_void()
+
+
+#----
+
+# Create a list of unique customer IDs and shop categories
+unique_customers <- unique(shop_id_S4 $customer_id)
+unique_categories <- unique(shop_id_S4 $shop_category)
+
+# Create an empty adjacency matrix with dimension names
+adj_matrix <- matrix(0, nrow = length(unique_customers), 
+                     ncol = length(unique_customers),
+                     dimnames = list(unique_customers, unique_customers))
+
+# Loop through the dataframe to update adjacency matrix
+for (i in 1:nrow(shop_id_S4 )) {
+  customer1 <- shop_id_S4 $customer_id[i]
+  category <- shop_id_S4 $shop_category[i]
+  
+  # Find other customers with the same category
+  other_customers <- shop_id_S4 $customer_id[shop_id_S4 $shop_category == category & 
+                                               shop_id_S4 $customer_id != customer1]
+  
+  # Update adjacency matrix for each pair of customers with the same category
+  adj_matrix[customer1, other_customers] <- 1
+}
+
+# Convert adjacency matrix to igraph object
+g <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected", weighted = TRUE)
+
+# Set diversity as node sizes
+V(g)$size <- shop_id_S4$diversity
+V(g)$colour <- as.factor (shop_id_S4$shop_category)
+
+
+# Plot with manual colors
+ggraph(g, layout = "fr") +
+  geom_edge_link(color = "white") +
+  geom_node_point(aes(size = size, colour = colour), alpha = 0.5) +
+  scale_size_continuous(range = c(0.5, 3)) + 
+  scale_colour_manual(values = category_colors) +
+  theme_void()
+
+#----
+# Create a list of unique customer IDs and shop categories
+unique_customers <- unique(shop_id_S5 $customer_id)
+unique_categories <- unique(shop_id_S5 $shop_category)
+
+# Create an empty adjacency matrix with dimension names
+adj_matrix <- matrix(0, nrow = length(unique_customers), 
+                     ncol = length(unique_customers),
+                     dimnames = list(unique_customers, unique_customers))
+
+# Loop through the dataframe to update adjacency matrix
+for (i in 1:nrow(shop_id_S5 )) {
+  customer1 <- shop_id_S5 $customer_id[i]
+  category <- shop_id_S5 $shop_category[i]
+  
+  # Find other customers with the same category
+  other_customers <- shop_id_S5 $customer_id[shop_id_S5 $shop_category == category & 
+                                               shop_id_S5 $customer_id != customer1]
+  
+  # Update adjacency matrix for each pair of customers with the same category
+  adj_matrix[customer1, other_customers] <- 1
+}
+
+# Convert adjacency matrix to igraph object
+g <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected", weighted = TRUE)
+
+# Set diversity as node sizes
+V(g)$size <- shop_id_S5 $diversity
+V(g)$colour <- as.factor (shop_id_S5$shop_category)
+
+
+# Plot with manual colors
+ggraph(g, layout = "fr") +
+  geom_edge_link(color = "white") +
+  geom_node_point(aes(size = size, colour = colour), alpha = 0.5) +
+  scale_size_continuous(range = c(0.5, 3)) + 
+  scale_colour_manual(values = category_colors) +
+  theme_void()
+
+
 
